6d5d17c863bc99e7f33cc27aed106cc6a54726cc
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2025-02-17 22:24:35 -0300

    chore: Melhorias na responsividade

--- resources/views/livewire/banner.blade.php
+++ resources/views/livewire/banner.blade.php
@@ -2,20 +2,23 @@
     {{-- The best athlete wants his opponent at his best. --}}
 
     <div class="w-full md:w-1/2 h-full flex flex-col justify-center items-center text-center">
-        <div class="font-iria-sans font-normal text-xl md:text-2xl text-black mb-6 md:mb-10">
-            Podologa Geriatrica
+        <div class="font-iria-sans font-normal text-xl md:text-2xl text-black mt-6 mb-4 md:mb-10">
+            Podologa
         </div>
 
         <div class="font-iria-sans font-normal text-xl md:text-2xl text-black mb-6 md:mb-10">
             em Balneário Camboriú
         </div>
 
-        <div class="bg-gradient-to-r from-[#BBB1F3] via-[#6DCCC4] to-[#5C5299] w-48 md:w-64 p-3 md:p-4 rounded-lg text-black font-iria-sans font-normal hover:opacity-80 hover:scale-105 transition-all duration-300 cursor-pointer flex items-center justify-center">
+        <a href="https://wa.me/47991626283?text=Olá%20Dra.%20Fabiana,%20gostaria%20de%20mais%20informações%20sobre%20agendamento%20de%20uma%20consulta%20avaliativa!" 
+        class="bg-gradient-to-r from-[#BBB1F3] via-[#6DCCC4] to-[#5C5299] w-48 md:w-64 p-3 md:p-4 rounded-lg text-black font-iria-sans font-normal hover:opacity-80 hover:scale-105 transition-all duration-300 flex items-center justify-center"
+        target="_blank">
             Agendar Consulta
-        </div>
+        </a>
+
     </div>
 
-    <div class="w-full md:w-1/2 flex justify-center mt-[95px] md:mt-[95px]">
+    <div class="w-full md:w-1/2 flex justify-center mt-[25px] sm:mt-[95px] md:mt-[95px]">
         <img class="w-40 sm:w-60 md:w-auto h-60 sm:h-60 md:h-[380px]" src="/assets/img/nova-logo.svg" alt="Logo da clinica - Doutora Fabiana Gonçalves">
     </div>
 </div>
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,4 +1,4 @@
 <div class="relative w-full h-[100px] bg-[#D9D9D9] flex items-center justify-center rounded-b-[10px] overflow-hidden z-30">
     <!-- Conteúdo Principal -->
-    <h1 class="text-[32px] font-bold text-black">Dra. Fabiana Gonçalves</h1>
+    <h1 class="text-[22px] sm:text-[32px] font-bold text-black">Dra. Fabiana Gonçalves</h1>
 </div>
